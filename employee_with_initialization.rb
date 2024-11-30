ed94d9e736b65a731dec452b4ad42b6358ba253e
ilya shaburov <assortimen@mail.ru>
2024-11-30 22:24:40 +0700

    Manage initialize method inside the classes

--- employee_with_initialization.rb
+++ employee_with_initialization.rb
@@ -1,2 +1,38 @@
-# frozen_string_literal: true
+#!/usr/bin/env ruby
 
+# Description: Инициализация экземпляров. Простой пример с инициализацией
+# Author:      jigick
+
+class Employee
+    attr_reader :name, :salary
+    def initialize(name='Anonymous', salary=1.0)
+        self.name = name # ОДНОЗНАЧНО вызывается метод «name=», в initialize лучше делать именно так,
+        self.salary = salary # ОДНОЗНАЧНО вызывается метод «salary=», в initialize лучше делать именно так!
+    end
+    def name=(name)
+        if name == ""
+            raise "Name can't be blank!!!"
+        end
+        @name = name
+    end
+    def salary=(salary)
+        if salary < 0
+            raise "A salary of #{salary} isn't valid!"
+        end
+        @salary = salary
+    end
+
+    def print_pay_stub
+        puts "Name: #{self.name}"
+        pay_for_period = (self.salary / 365.0) * 14
+        formatted_pay = format("$%.2f", pay_for_period)
+        puts "Payment This Period: #{formatted_pay}"
+    end
+end
+
+
+new_employee = Employee.new('NEWCOMER')
+new_employee.print_pay_stub
+
+real_employee = Employee.new('Afanasy', 10000)
+real_employee.print_pay_stub
--- employee_without_initialization.rb
+++ employee_without_initialization.rb
@@ -1,2 +1,49 @@
-# frozen_string_literal: true
+#!/usr/bin/env ruby
 
+# frozen_string_literal: true
+# Description: Инициализация экземпляров. Пример без инициализации.
+# Author:      jigick
+
+class Employee
+    attr_reader :name, :salary
+
+    def initialize
+        puts "Setting up new instance!"  # the simplest example of initialize
+    end
+
+    def name=(name)
+
+        if name == ""
+            raise "Name can't be blank!"
+        end
+        @name = name
+    end
+    def salary=(salary)
+        if salary < 0
+            raise "A salary of #{salary} isn't valid!"
+        end
+            @salary = salary
+    end
+    def print_pay_stub
+        puts "Name: #{@name}, Salary: #{@salary}"
+        p @name, @salary # inspected
+        pay_for_period = (@salary.to_f / 365.0) * 14
+        puts format('Pay This Period: $ %0.2f', pay_for_period) # float output formatting inside the strings
+    end
+
+end
+
+amy = Employee.new
+amy.name = "Amy Blake"
+amy.salary = 50000
+
+amy.print_pay_stub
+
+kara = Employee.new
+kara.name = "Kara Byrd"
+kara.salary = 45000
+
+kara.print_pay_stub
+
+kirill = Employee.new
+kirill.print_pay_stub
